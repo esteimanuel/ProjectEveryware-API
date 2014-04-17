035c4e8efcc55001844e382a9934111751b82eea
Gijs van der Venne <gijs.venne@hotmail.com>
2014-04-17 20:29:32 +0200

    probeersel nested view (werkt niet somehow)

--- web/app/app.js
+++ web/app/app.js
@@ -2,10 +2,10 @@
  * Main application init
  */
 console.log("app boot script");
-var app = angular.module(config.app.name,['ui.router', 'ui.bootstrap'])
-.config(function($locationProvider, $stateProvider, $urlRouterProvider) {
+var app = angular.module(config.app.name, ['ui.router', 'ui.bootstrap'])
+.config(function ($locationProvider, $stateProvider, $urlRouterProvider) {
     //$locationProvider.html5Mode(true);
-    
+
     $stateProvider.state('#', {
         url: '',
         views: {
@@ -24,38 +24,48 @@ var app = angular.module(config.app.name,['ui.router', 'ui.bootstrap'])
         views: {
             "left-nav": { templateUrl: 'app/left-nav/nav.html' },
             "main-content": { templateUrl: 'app/faq/faq.html' }
-    }
+        }
+    })
+    .state('mijnWijk', {
+        url: '/mijnWijk',
+        views: {
+            "left-nav": { templateUrl: 'app/left-nav/nav.html' },
+            "main-content": { templateUrl: 'app/wijk-actie/mijn-wijk.html' },
+        }
+    })
+    .state('mijnWijk.progress', {
+        "progress": { templateUrl: 'app/wijk-actie/mijn-wijk.progress.html' }
     });
 
-//    .state('login', {
-//        url:'/login',
-//        title: 'Login',
-//        views: {
-//            "dialog":{templateUrl:'templates/login.html' /*, controller:'UserCtrl'*/}
-//        }
-//    }).state('register', {
-//        url:'/register',
-//        title: 'Register',
-//        views: {
-//            "dialog":{templateUrl:'templates/register.html' /*, controller:'UserCtrl'*/}
-//        }
-//    }).state('main.board.edittask', {
-//        url:'/task/:taskid',
-//        views: {
-//            "main-dialog":{templateUrl: 'templates/edittask.html', controller:'BoardCtrl'}
-//        }
-//    }).state('main.board',{
-//        url:'/b/{boardid}',
-//        views: {
-//            "tab-content":{templateUrl:'templates/board.html',controller:'BoardCtrl'}
-//        }
-//    });
-    
+    //    .state('login', {
+    //        url:'/login',
+    //        title: 'Login',
+    //        views: {
+    //            "dialog":{templateUrl:'templates/login.html' /*, controller:'UserCtrl'*/}
+    //        }
+    //    }).state('register', {
+    //        url:'/register',
+    //        title: 'Register',
+    //        views: {
+    //            "dialog":{templateUrl:'templates/register.html' /*, controller:'UserCtrl'*/}
+    //        }
+    //    }).state('main.board.edittask', {
+    //        url:'/task/:taskid',
+    //        views: {
+    //            "main-dialog":{templateUrl: 'templates/edittask.html', controller:'BoardCtrl'}
+    //        }
+    //    }).state('main.board',{
+    //        url:'/b/{boardid}',
+    //        views: {
+    //            "tab-content":{templateUrl:'templates/board.html',controller:'BoardCtrl'}
+    //        }
+    //    });
+
     $urlRouterProvider.otherwise("");
-    
+
 })
-.run(function($rootScope, $state) {
-    $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState) {
+.run(function ($rootScope, $state) {
+    $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState) {
         console.log("State change");
         console.log(toState);
         console.log(fromState);
