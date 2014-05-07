464321842f3957a84cdc772fe682b4c60368ab1b
Remi Sloot <Remi Sloot@RemiSloot-PC>
2014-05-07 15:09:11 +0200

    API postalcode functie nu wel in postcode controller :P

--- api/controllers/PostcodeController.php
+++ api/controllers/PostcodeController.php
@@ -5,6 +5,17 @@ class PostcodeController extends BaseController {
 	public function addToWijk() {
 		
 	}
+        
+    public function forDistrict() {
+        $wijk_id = $this->request->getQuery('id');
+        $postalCodes = null;
+        if(isset($wijk_id) && $wijk_id > 0) {
+            $postalCodes = Postcode::find(array('wijk_id' => $wijk_id));
+        } else {
+            $this->response->setStatusCode(400, "Invalid or no id");
+        }
+        $this->response->setJsonContent(array('postalcodes' => $postalCodes));
+    }
 
 }
 
--- api/controllers/WijkController.php
+++ api/controllers/WijkController.php
@@ -6,13 +6,13 @@ class WijkController extends BaseController {
 
     }
 
-    public function postalcodes() {
-        $wijk_id = $this->request->getQuery('id');
-        if(isset($wijk_id) && $wijk_id > 0) {
-            $wijk = Wijk::findFirst($wijk_id);
-            $this->response->setJsonContent(array('messages' => $messages, 'postalcodes' => $wijk->Postcode));
-        }
-    }
+//    public function postalcodes() {
+//        $wijk_id = $this->request->getQuery('id');
+//        if(isset($wijk_id) && $wijk_id > 0) {
+//            $wijk = Wijk::findFirst($wijk_id);
+//            $this->response->setJsonContent(array('messages' => $messages, 'postalcodes' => $wijk->Postcode));
+//        }
+//    }
 }
 
 ?>
