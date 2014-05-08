d5082174f2fc508036792d52e626d9987ab07bf5
Remi Sloot <Remi Sloot@RemiSloot-PC>
2014-05-08 10:40:29 +0200

    Actie users

--- api/controllers/ActieController.php
+++ api/controllers/ActieController.php
@@ -2,9 +2,26 @@
 
 class ActieController extends BaseController {
 
-	public function addUser() {
-		
-	}
+    public function addUser() {
+
+    }
+        
+    public function users() {
+        $actie_id = $this->request->getQuery('id');
+        if(isset($actie_id) && $actie_id > 0) {
+            $data = User::find(array('actie_id' => $actie_id));
+            $users = array();
+            foreach($data as $user) {
+                $user->account = $user->getAccount();
+                $user->buddy = $user->getBuddy();
+                $users[] = $user;
+            }
+            
+            $this->response->setJsonContent($users);
+        } else {
+            $this->response->setStatusCode(400, 'Bad value given');
+        }
+    }
 
 }
 
