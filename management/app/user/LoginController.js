e94defe9cffd9e3a456b95ce7a9446ca7044949e
pcschool <pcschool@WIN7X64V1-010.aii.avans.nl>
2014-05-08 12:55:18 +0200

    Veel shit

--- management/app/user/LoginController.js
+++ management/app/user/LoginController.js
@@ -1,12 +1,21 @@
-app.controller('LoginCtrl', function($scope, $http) {
-	$scope.login = function(credentials) {
-		console.log(credentials);
-		$http({url: 'http://glassy-api.avans-project.nl/api/account/login',method:'GET',params:credentials}).success(function(data, status) {
-			console.log("Token has been received");
-			console.log(data);
-			localStorage.token = data.token;
-		}).error(function(data, status, headers, config) {
-			console.log("Login file data not accepted");
-		})
-	}
+app.controller('LoginCtrl', function($scope, $http, $state, User) {
+    $scope.login = function(credentials) {
+            console.log(credentials);
+            $http({url: config.api.url + 'account/login',method:'GET',params:credentials}).success(function(data, status) {
+                    console.log("Token has been received");
+                    localStorage.token = data.account.token;
+                    User.data.isLogged = data.account.token;
+                    User.data.token = data.account.token;
+                    $state.transitionTo('main.dashboard');
+            }).error(function(data, status, headers, config) {
+                    console.log("Login file data not accepted");
+            })
+    }
+    
+    $scop.logout = function(){
+        localStorage.removeItem(token);
+        User.data.isLogged = false;
+        User.data.token = null;
+        $state.transitionTo('/login');
+    }
 });
