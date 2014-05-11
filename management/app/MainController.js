dead432a5f3ad9807b6381be710035b4eb80d5ce
pschool <Pieter@PC-Pieter>
2014-05-12 00:20:05 +0200

    Login vernieuwd en funties in de maincontroller geplaatst

--- management/app/MainController.js
+++ management/app/MainController.js
@@ -11,15 +11,15 @@ app.controller('MainCtrl', function($scope, $http, $state, User) {
     };
     
     $scope.login = function(credentials) {
-            console.log(credentials);
             $http({url: config.api.url + 'account/login',method:'GET',params:credentials}).success(function(data, status) {
-                    console.log("Token has been received");
-                    localStorage.token = data.account.token;
-                    User.data.isLogged = data.account.token;
-                    User.data.token = data.account.token;
-                    $state.transitionTo('main.dashboard.districts');
+                console.log("Token has been received");
+                localStorage.token = data.account.token;
+                User.data.isLogged = data.account.token;
+                User.data.token = data.account.token;
+                $state.transitionTo('main.dashboard.districts');
             }).error(function(data, status, headers, config) {
-                    console.log("Login file data not accepted");
+                console.log("Login file data not accepted");
+                $scope.errorMessage = "Inloggegevens zijn incorrect";
             });
     };
     
