561e8a76f80153b99a8d693a94fdccf72506864e
Remi <Remi@192.168.1.102>
2014-05-15 17:12:51 +0200

    Faq C

--- web/app/faq/FaqController.js
+++ web/app/faq/FaqController.js
@@ -2,7 +2,7 @@ app.controller('FaqCtrl', function ($scope, $rootScope) {
     console.log("faq loaded");
 
     $scope.getFaq = function () {
-        çonsole.log("i got to the method");
+        console.log("i got to the method");
 
         $http({
             url: config.api.url + 'faq',
