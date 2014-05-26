54a3cad77359f6df721fe1b93687b741166dff43
Gijs van der Venne <gijs.venne@hotmail.com>
2014-05-26 12:22:53 +0200

    test

--- management/app/district/addDistrict.js
+++ management/app/district/addDistrict.js
@@ -57,21 +57,23 @@ app.controller('addDistrictCtrl', function($scope, $http, $timeout, $state, $sce
             return;
         }
         
-        addWijkIfNotEist(wijk)
+        addWijkIfNotEist(wijk, postcode)
         
         if(addSingle){
             if(!rege.test(postcode.single)){
                 alert("De postcode voldoet niet enkel as voorbeeld \r\n\r\n 1111AA");
                 return;
             }
-            $timeout(AddSingleZip(postcode.single), 2000);
+
+        if(!$scope.currentWijkId){
+        var result = $timeout(AddSingleZip(postcode.single), 2000);}
         }
         
         //Reload Map
         $scope.mapsUrl = $sce.trustAsResourceUrl("http://glassy-web.avans-project.nl/?wijk=" + $scope.currentWijkId);
     };
    
-   function addWijkIfNotEist(wijk){
+   function addWijkIfNotEist(wijk, postcode){
         //If wijk not set add, else update
         if(!$scope.currentWijkId){
             var body = {wijk_naam: wijk.name, beschikbaar: wijk.avalible, target: wijk.target, actie_duur_dagen: wijk.duration, aantal_huishoudens: wijk.totalHousholds};
@@ -86,6 +88,7 @@ app.controller('addDistrictCtrl', function($scope, $http, $timeout, $state, $sce
                 $scope.currentWijkId = data.id;
                 console.log($scope);
                 alert($scope.currentWijkId);
+                AddSingleZip(postcode.single)
                 })
                 .error(function(data, status, headers, config){
                 alert('Gegevens konden niet opgeslagen worden.');
@@ -111,13 +114,14 @@ app.controller('addDistrictCtrl', function($scope, $http, $timeout, $state, $sce
         var body = {"postalcode": Zip, "wid": $scope.currentWijkId};
         var url = config.api.url+'postcode/editDistrictId';
         
+        console.log(body);
+
         $http({
             url:url,
             method:"PUT",
             data: body
         }).success(function (data, status, headers, config) {
             console.log(data);
-            console.log(headers);
             alert('succes');    
             })
             .error(function(data, status, headers, config){
