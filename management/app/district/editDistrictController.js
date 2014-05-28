2e457f8826a46bd46849b5ae6b7c22b1bf3a7374
pieter <pieter@145.102.95.6>
2014-05-28 14:08:02 +0200

    Hoodletterfout change bij single postcode

--- management/app/district/editDistrictController.js
+++ management/app/district/editDistrictController.js
@@ -158,7 +158,7 @@ app.controller('editDistrictCtrl', function($scope, $http, $timeout, $state, $sc
         //Update in API
         var body = {"postalcode": Zip, "wid": $scope.currentWijkId};
         console.log(body);
-        var url = config.api.url+'postcode/ChangeDistrictId';
+        var url = config.api.url+'postcode/changeDistrictId';
         
         console.log(body);
 
