4994e62ebbf95d01acf81af83daa0424fc467c3d
pschool <Pieter@PC-Pieter>
2014-06-02 01:59:34 +0200

    actie edit display users

--- management/app/actie/editActieController.js
+++ management/app/actie/editActieController.js
@@ -5,12 +5,28 @@
 
 app.controller('editActieCtrl', function($scope, $http, $timeout, $state, $sce, $stateParams) {    
     //Get the district to edit
-    $scope.currentActieId = $stateParams.aid;
+    $scope.actieId = $stateParams.aid;
     GetDataForActie();    
+    GetActieDeelnemers();
+    
+    //Table config
+    $scope.rows = [];
+    $scope.headers = [
+        {name:"voornaam", type:"text", header:"Voornaam"},
+        {name:"tussenvoegsel", type:"text", header:"Tussenvoegsels"},
+        {name:"achternaam", type:"text", header:"Achternaam"},
+        {name:"borg_betaald", type:"checkbox", header:"Borg betaald"},
+        {name:"postcode_id", type:"text", header:"Postcode"},
+        {name:"huisnummer", type:"text", header:"Huisnummer"}
+    ];
+    
+    //Disable modifiers for table
+    $scope.allowEdit = false;
+    $scope.allowDelete = false;
     
     ///Gets all data for selected action
     function GetDataForActie(){
-        var url = config.api.url + 'actie?id=' + $scope.currentActieId;
+        var url = config.api.url + 'actie?id=' + $scope.actieId;
         $http({
             url: url,
             method: 'GET'
@@ -39,7 +55,7 @@ app.controller('editActieCtrl', function($scope, $http, $timeout, $state, $sce, 
             url: url,
             method: 'GET'
         }).success(function(data, status, headers, config) {
-            $scope.wijkName = data.Zwanenveld;        
+            $scope.wijkName = data.wijk_naam;        
             $scope.wijkTotal = data.aantal_huishoudens;        
             $scope.wijkTarget = data.target;        
             $scope.wijkDuration = data.actie_duur_dagen;        
@@ -61,6 +77,20 @@ app.controller('editActieCtrl', function($scope, $http, $timeout, $state, $sce, 
             console.log("Postcode load failed");
         });
     }
+    
+    function GetActieDeelnemers(){
+        var url = config.api.url+'actie/users?id=' + $scope.actieId;
+        $http({
+            url: url,
+            method: 'GET'
+        }).success(function(data, status, headers, config) {
+            console.log("Actie data load succesfull");
+            $scope.rows = data;
+            $scope.draw();
+        }).error(function(data, status, headers, config) {
+            console.log("Actie data load failed /r/n/r/n Reson: /r/n" + headers);
+        });
+    }; 
    
    function loadMap(){       
         $scope.mapsUrl = $sce.trustAsResourceUrl("http://glassy-web.avans-project.nl/?wijk=" + $scope.wijkId);
