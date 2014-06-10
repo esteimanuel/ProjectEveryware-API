8cf277d6ef3834447a72767b63f3597d79c55b8b
Gijs van der Venne <gijs.venne@hotmail.com>
2014-06-10 12:50:16 +0200

    api functie maybe

--- api/controllers/BuddyController.php
+++ api/controllers/BuddyController.php
@@ -5,9 +5,16 @@ class BuddyController extends BaseController {
 public function put() {
         $putData = $this->getRequestData();
         if(isset($putData->gebruiker_id)) {
-            unset($putData['_token']);
+            $model = Buddy::findFirst($putData->gebruiker_id);
+            
+            if($model) {
+              unset($putData['_token']);
+              unset($putData['gebruiker_id']);
+              
+              $model->save();
+            }
+
             $messages = '';
-            $model = $this->basePut($putData, $messages);
             $this->response->setJsonContent(array('messages' => $messages, 'model' => $model));
         } else {
             $this->response->setStatusCode(404, "No Account Found");
