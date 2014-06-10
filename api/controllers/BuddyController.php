a53a2e5dc7178a5365ac3f76593e94fd9379c205
Gijs van der Venne <gijs.venne@hotmail.com>
2014-06-10 13:17:08 +0200

    api aanpassing not working

--- api/controllers/BuddyController.php
+++ api/controllers/BuddyController.php
@@ -5,16 +5,24 @@ class BuddyController extends BaseController {
 public function put() {
         $putData = $this->getRequestData();
         if(isset($putData->gebruiker_id)) {
+           
+            $model = Buddy::findFirst($putData['gebruiker_id']);
+            
             unset($putData['_token']);
             unset($putData['gebruiker_id']);
-            
-            $model = $putData;
+            $messages = '';
             
             if($model) {
-              $model->save();
+
+              foreach($putData as $key => $value) {
+                $model->$key = $value;
+              }
+              
+              if(!$model->save()) {
+                $messages = $this->checkErrors($model);
+              }
             }
 
-            $messages = '';
             $this->response->setJsonContent(array('messages' => $messages, 'model' => $model));
         } else {
             $this->response->setStatusCode(404, "No Account Found");
