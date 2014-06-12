850e6023525651442aabb510883d8ff3709e99fc
Mathijs <1994.mathijs@gmail.com>
2014-06-13 01:48:04 +0200

    nog een commit van mij! hopelijk werkt het nu wel...

--- api/controllers/BuddyController.php
+++ api/controllers/BuddyController.php
@@ -2,38 +2,41 @@
 
 class BuddyController extends BaseController {
 
-    public function put() {
-      $putData = $this->getRequestData();
-      if(isset($putData->gebruiker_id)) {
-         
-          $model = Buddy::findFirst($putData['gebruiker_id']);
-          
-          unset($putData['_token']);
-          unset($putData['gebruiker_id']);
-          $messages = '';
-          
-          if($model) {
+  public function put() {
+    $putData = $this->getRequestData();
+    if(isset($putData->gebruiker_id)) {
+       
+        $model = Buddy::findFirst($putData['gebruiker_id']);
+        
+        unset($putData['_token']);
+        unset($putData['gebruiker_id']);
+        $messages = '';
+        
+        if($model) {
 
-            foreach($putData as $key => $value) {
-              $model->$key = $value;
-            }
-            
-            if(!$model->save()) {
-              $messages = $this->checkErrors($model);
-            }
+          foreach($putData as $key => $value) {
+            $model->$key = $value;
           }
+          
+          if(!$model->save()) {
+            $messages = $this->checkErrors($model);
+          }
+        }
 
-          $this->response->setJsonContent(array('messages' => $messages, 'model' => $model));
-      } else {
-          $this->response->setStatusCode(404, "No Account Found");
-      }
+        $this->response->setJsonContent(array('messages' => $messages, 'model' => $model));
+    } else {
+        $this->response->setStatusCode(404, "No Account Found");
+    }
   }
 
   public function getByGebruikerId() {
 
     $sGebruikerId = $this->request->getQuery('gebruiker_id');
 
-    $model = Buddy::findFirst($sGebruikerId);
+    $model = Buddy::findFirst(array(
+                    'conditions' => 'gebruiker_id = :id:',
+                    'bind' => array('id' => $sGebruikerId),
+                ));
 
     if($model) {
 
@@ -44,7 +47,3 @@ class BuddyController extends BaseController {
     }
   }
 }
-
-  
-
-?>
