07914a40186e5d4f19bb262ad149b30e980d15bf
Mathijs <1994.mathijs@gmail.com>
2014-06-13 01:43:59 +0200

    jeej ik werk ook aan de api...

--- api/controllers/BuddyController.php
+++ api/controllers/BuddyController.php
@@ -2,33 +2,32 @@
 
 class BuddyController extends BaseController {
 
-public function put() {
-        $putData = $this->getRequestData();
-        if(isset($putData->gebruiker_id)) {
-           
-            $model = Buddy::findFirst($putData['gebruiker_id']);
-            
-            unset($putData['_token']);
-            unset($putData['gebruiker_id']);
-            $messages = '';
-            
-            if($model) {
+    public function put() {
+      $putData = $this->getRequestData();
+      if(isset($putData->gebruiker_id)) {
+         
+          $model = Buddy::findFirst($putData['gebruiker_id']);
+          
+          unset($putData['_token']);
+          unset($putData['gebruiker_id']);
+          $messages = '';
+          
+          if($model) {
 
-              foreach($putData as $key => $value) {
-                $model->$key = $value;
-              }
-              
-              if(!$model->save()) {
-                $messages = $this->checkErrors($model);
-              }
+            foreach($putData as $key => $value) {
+              $model->$key = $value;
             }
+            
+            if(!$model->save()) {
+              $messages = $this->checkErrors($model);
+            }
+          }
 
-            $this->response->setJsonContent(array('messages' => $messages, 'model' => $model));
-        } else {
-            $this->response->setStatusCode(404, "No Account Found");
-        }
-    }
-}
+          $this->response->setJsonContent(array('messages' => $messages, 'model' => $model));
+      } else {
+          $this->response->setStatusCode(404, "No Account Found");
+      }
+  }
 
   public function getByGebruikerId() {
 
@@ -44,5 +43,8 @@ public function put() {
       $this->response->setStatusCode(404, "Buddy niet gevonden, Koos Vriendloos!");
     }
   }
+}
+
+  
 
 ?>
